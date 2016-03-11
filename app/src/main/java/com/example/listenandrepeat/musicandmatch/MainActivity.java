cbd86e04ea8b5765edf94be23e2a06f3146772e0
Tacademy <Tacademy@example.com>
2016-03-11 14:37:44 +0900

    dgdgg

--- app/src/main/java/com/example/listenandrepeat/musicandmatch/MainActivity.java
+++ app/src/main/java/com/example/listenandrepeat/musicandmatch/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.listenandrepeat.musicandmatch;
 
 
-import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentManager;
@@ -9,16 +9,14 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
-
+    // inhohohoho
     NavigationView menuView;
     DrawerLayout drawer;
     TabLayout tabLayout;
