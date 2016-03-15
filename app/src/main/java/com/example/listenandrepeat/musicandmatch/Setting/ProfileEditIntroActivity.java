1ee02d933216097cb652fbbffde185767c5a3636
Tacademy <Tacademy@example.com>
2016-03-15 22:00:10 +0900

    toolbar

--- app/src/main/java/com/example/listenandrepeat/musicandmatch/Setting/ProfileEditIntroActivity.java
+++ app/src/main/java/com/example/listenandrepeat/musicandmatch/Setting/ProfileEditIntroActivity.java
@@ -2,8 +2,10 @@ package com.example.listenandrepeat.musicandmatch.Setting;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.listenandrepeat.musicandmatch.R;
 
@@ -13,6 +15,16 @@ public class ProfileEditIntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_edit_intro);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle("소개글 편집");
+        toolbar.setNavigationIcon(R.drawable.ic_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ProfileEditIntroActivity.this, "center toolbar navigation click", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         Button btn = (Button)findViewById(R.id.btn_change);
         btn.setOnClickListener(new View.OnClickListener() {
